f27e18826ba1b2dc2112d9bcecb965ea09def5bf
ardencollier1297 <79470715+ardencollier1297@users.noreply.github.com>
2021-02-23 14:27:24 -0500

    Update AssignmentOne.java
    Re-done

--- AssignmentOne/AssignmentOne.java
+++ AssignmentOne/AssignmentOne.java
@@ -1,14 +1,125 @@
 package com.arden;
 
+
 public class AssignmentOne {
+
 
 	public static void main(String[] args) {
 		// Day One Assignment One - Java
+		String star = "*";
 		
-		System.out.println("1)\n*\n**\n***\n****\n.........");
-		System.out.println("\n2)\n..........\n****\n***\n**\n*");
-		System.out.println("\n3)\n     *\n    ***\n   *****\n  *******\n...........");
-		System.out.println("\n4)\n............\n  *******\n   *****\n    ***\n     *");
-	}
+		firstPattern(star);
+		secondPattern(star);
+		thirdPattern(star);
+		fourthPattern(star);
+		}
+
+		public static void firstPattern(String star) {
+			
+			StringBuffer stars = new StringBuffer(star);
+			System.out.println("1)\n");
+			for(int i = 0; i < 4; i++) {
+				System.out.println(stars);
+				stars.append(star);
+				
+			}
+			StringBuffer dots = new StringBuffer("");
+			for(int i = 0; i < 9; i++) {
+				dots.append(".");
+			}
+			System.out.println(dots + "\n");
+		}
+		
+		public static void secondPattern(String star) {
+
+			StringBuffer stars = new StringBuffer(star);
+			System.out.println("2)\n");
+			for(int i = 0; i < 3; i++) {
+				stars.append(star);
+			}
+			System.out.println(stars);
+			for(int i = 0; i <= 2; i++) {
+				stars.setLength(stars.length() -1);
+				System.out.println(stars);
+			}
+			StringBuffer dots = new StringBuffer("");
+			for(int i = 0; i < 10; i++) {
+				dots.append(".");
+			}
+			System.out.println(dots + "\n");
+		}
+		
+		public static void thirdPattern(String star) {
+			
+			StringBuffer stars = new StringBuffer("");
+			System.out.println("3)\n");
+			for(int i = 0; i < 5; i++) {
+				stars.append(" ");
+			}
+			stars.append(star + "\n");
+			for(int i = 0; i < 4; i++) {
+				stars.append(" ");
+			}
+			for(int i = 0; i < 3; i++) {
+				stars.append(star);
+			}
+			stars.append("\n");
+			for(int i = 0; i < 3; i++) {
+				stars.append(" ");
+			}
+			for(int i = 0; i < 5; i++) {
+				stars.append(star);
+			}
+			stars.append("\n");
+			for(int i = 0; i < 2; i++) {
+				stars.append(" ");
+			}
+			for(int i = 0; i < 7; i++) {
+				stars.append(star);
+			}
+			StringBuffer dots = new StringBuffer("");
+			for (int i = 0; i < 11; i++) {
+				dots.append(".");
+			}
+			System.out.println(stars);
+			System.out.println(dots + "\n");
+		}
+		
+		public static void fourthPattern(String star) {
+			
+			StringBuffer stars = new StringBuffer("");
+			System.out.println("4)\n");
+			StringBuffer dots = new StringBuffer("");
+			for (int i = 0; i < 12; i++) {
+				dots.append(".");
+			}
+			for(int i = 0; i < 2; i++) {
+				stars.append(" ");
+			}
+			for(int i = 0; i < 7; i++) {
+				stars.append(star);
+			}
+			stars.append("\n");
+			for(int i = 0; i < 3; i++) {
+				stars.append(" ");
+			}
+			for(int i = 0; i < 5; i++) {
+				stars.append(star);
+			}
+			stars.append("\n");
+			for(int i = 0; i < 4; i++) {
+				stars.append(" ");
+			}
+			for(int i = 0; i < 3; i++) {
+				stars.append(star);
+			}
+			stars.append("\n");
+			for(int i = 0; i < 5; i++) {
+				stars.append(" ");
+			}
+			stars.append(star);
+			System.out.println(dots);
+			System.out.println(stars);
+		}
 
 }
